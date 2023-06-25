71274a0e3ceb0ba9be20b5ca4cc96679a2bd2e5b
dangeonMaster <dafae722@gmail.com>
2023-06-26 01:22:37 +0300

    initial commit

---
+++ src/main/java/ru/clevertec/ecl/clevertecfinaltask/ClevertecFinalTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.ecl.clevertecfinaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClevertecFinalTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClevertecFinalTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/clevertec/ecl/clevertecfinaltask/ClevertecFinalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.ecl.clevertecfinaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClevertecFinalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
